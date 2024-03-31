1e9303e148fd6c1401049be37db5c364a3859ac3
Ahmad Fakhar <155258276+Ahmad-Fakhar@users.noreply.github.com>
2024-03-31 21:58:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.chatbot.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private final String BOT_KEY = "bot", USER_KEY = "user";
+    ArrayList<ChatsModel> chatsModelArrayList;
+    private ChatRvAdapter chatRvAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.idEdtMessage.requestFocus();
+        chatsModelArrayList = new ArrayList<>();
+        chatRvAdapter = new ChatRvAdapter(chatsModelArrayList,this);
+
+        //vertical orientation
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        binding.idRVChats.setLayoutManager(manager);
+
+        binding.idRVChats.setAdapter(chatRvAdapter);
+
+        binding.idFABSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.idEdtMessage.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please enter some message", Toast.LENGTH_SHORT).show();
+                }else{
+                    getResponse(binding.idEdtMessage.getText().toString());
+                    binding.idEdtMessage.setText("");
+                }
+            }
+
+
+        });
+
+
+
+    }
+
+    private void getResponse(String message) {
+        chatsModelArrayList.add(new ChatsModel(message,USER_KEY));
+        chatRvAdapter.notifyDataSetChanged();
+
+        String url = "AIzaSyCP_Xw6y_WbVju4Nj37COKoRKD10Vzvw1E"+message;
+        String BASE_URL = "https://aistudio.google.com/app/apikey";
+        Retrofit retrofit =new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+        Call<MsgModel> call = retrofitAPI.getMessage(url);
+        call.enqueue(new Callback<MsgModel>() {
+            @Override
+            public void onResponse(Call<MsgModel> call, Response<MsgModel> response) {
+                if(response.isSuccessful()){
+                    MsgModel model = response.body();
+                    chatsModelArrayList.add(new ChatsModel(model.getCnt(),BOT_KEY));
+                    chatRvAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MsgModel> call, Throwable t) {
+                chatsModelArrayList.add(new ChatsModel("Please revert your questions",BOT_KEY));
+                chatRvAdapter.notifyDataSetChanged();
+
+            }
+        });
+    }
+
+}
